4695cd51d3e987ad3c76c5e2101f8abdf15a6aa8
Dian van Deventer <dian.vandeventer@gmail.com>
2021-02-22 18:51:14 +0200

    Functionality, style, robustness, best practices updatetd.

--- Sequential.java
+++ Sequential.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 /**
  * @author Werner
@@ -30,11 +28,23 @@ interface NumberRangeSummarizer {
 }
 
 public class Sequential implements NumberRangeSummarizer {
-    static Collection<Integer> col = new ArrayList<>();
-    static String answer = "";
+    private Collection<Integer> col;
+    private String answer;
+
+    public Sequential() {
+        this.col = new ArrayList<>();
+        this.answer = "";
+    }
+
+    public Collection<Integer> getCol() {
+        return this.col;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
 
     public static void main(String... args) {
-
         Sequential seq = new Sequential();
         String userIn = "";
         Scanner keyboard = new Scanner(System.in);
@@ -48,38 +58,29 @@ public class Sequential implements NumberRangeSummarizer {
             if (!(userIn.matches("^[1-9][0-9]*"))) {
                 System.out.println("Invalid number");
             } else {
-
-                col = seq.collect(userIn);
+                seq.col = seq.collect(userIn);
             }
         }
-        keyboard.close();
-        answer = seq.summarizeCollection(col);
-        System.out.println(answer);
 
+        keyboard.close();
+        seq.answer = seq.summarizeCollection(seq.col);
+        System.out.println(seq.answer);
     }
 
     @Override
     public Collection<Integer> collect(String input) {
-        // System.out.println(input);
-
-        col.add(Integer.parseInt(input));
-
-        return col;
+        this.col.add(Integer.parseInt(input));
+        return this.col;
     }
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-
-        // int[] myArrList = { 1, 3, 6, 7, 8, 12, 13, 14, 15, 21, 22, 23, 24, 31 };
-
         int counter = 0;
         int colSize = input.size();
-        // System.out.println("col: " + colSize);
         int[] myArrList = new int[colSize];
+
         for (int i = 0; i <= colSize - 1; i++) {
-            // System.out.println("Array before: " + myArrList[counter]);
             myArrList[counter] = (int) input.toArray()[counter];
-            // System.out.println("Array after: " + myArrList[counter]);
             counter++;
         }
 
@@ -91,17 +92,15 @@ public class Sequential implements NumberRangeSummarizer {
             while (++number2 < arrListLength && myArrList[number2] - myArrList[number2 - 1] == 1)
                 ;
             if (number2 - number > 2) {
-                // System.out.printf("%s-%s,", myArrList[number], myArrList[number2 - 1]);
-                answer = answer + String.format("%s-%s,", myArrList[number], myArrList[number2 - 1]);
+                this.answer = this.answer + String.format("%s-%s,", myArrList[number], myArrList[number2 - 1]);
                 number = number2;
             } else {
                 for (; number < number2; number++)
-                    answer = answer + String.format("%s,", myArrList[number]);
-                // System.out.printf("%s,", myArrList[number]);
+                    this.answer = this.answer + String.format("%s,", myArrList[number]);
             }
         }
 
-        return answer;
+        return this.answer;
     }
 
 }
